b1955b6ae2ebfbeb46663815e2c38206dbf3d081
redrumlivedog@outlook.com <redrumlivedog@outlook.com>
2022-08-02 11:01:45 +0800

    add simple stack

---
+++ Stack/SqStack.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+#define Elemtype int
+#define MaxSize 10
+
+struct SqStack
+{
+    Elemtype Stack[MaxSize];
+    int top;
+};
+
+void InitStack(SqStack &S)
+{
+    S.top = -1;
+}
+
+bool Push(SqStack &S, Elemtype e)
+{
+    if (S.top == MaxSize - 1) { return false; }
+
+    S.top += 1;
+    S.Stack[S.top] = e;
+    // S.Stack[++S.top] = e;
+
+    return true;
+}
+
+bool Pop(SqStack &S, Elemtype &e)
+{
+    if (S.top == -1) { return false; }
+
+    e = S.Stack[S.top];
+    S.top -= 1;
+    // e = S.Stack[S.top--];
+
+    return true;
+}
+
+bool Empty(SqStack &S)
+{
+    if (S.top == -1) { return true; }
+
+    return false;
+}
+
+int main()
+{
+    int i = 0;
+    int m = 0;
+    SqStack S;
+    InitStack(S);
+
+    while (Push(S, ++i))
+    {
+        printf("pushing\n");
+    }
+
+    while (Pop(S, m))
+    {
+        printf("%d\n", m);
+    }
+}
